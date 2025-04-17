a3571e85307ea1b45f5660be66ed18cc6e6c23f8
David-Furstner <furstnerdavid@gmail.com>
2025-04-17 17:28:17 +0200

    Invoice jól fut

--- HotelGuru/WebApp/blueprints/invoice/schemas.py
+++ HotelGuru/WebApp/blueprints/invoice/schemas.py
@@ -4,12 +4,20 @@ class InvoiceRequestSchema(Schema):
     reservation_id = fields.Integer(required=True)
     issue_date = fields.Date(required=True)
 
+class InvoiceItemSchema(Schema):
+    id = fields.Integer()
+    item_type = fields.String()
+    item_id = fields.Integer()
+    name = fields.String()
+    price = fields.Float()
+    quantity = fields.Integer()
 
 class InvoiceResponseSchema(Schema):
     id = fields.Integer()
     reservation_id = fields.Integer()
     issue_date = fields.Date()
     total_amount = fields.Float()
+    items = fields.List(fields.Nested(InvoiceItemSchema))
 
 class InvoiceUpdateSchema(Schema):
     total_amount = fields.Float()
