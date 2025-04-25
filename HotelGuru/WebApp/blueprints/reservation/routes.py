8cba24e11faaf326d2246831d7c25f7598b8a711
almaflex10 <ormos.barnabas10@gmail.com>
2025-04-25 11:23:42 +0200

    reservation auth is meg van

--- HotelGuru/WebApp/blueprints/reservation/routes.py
+++ HotelGuru/WebApp/blueprints/reservation/routes.py
@@ -3,7 +3,8 @@ from WebApp.blueprints.reservation.schemas import  ReservationRequestSchema, Res
 from WebApp.blueprints.reservation.service import ReservationService
 
 from apiflask import HTTPError
-
+from WebApp.extensions import auth
+from WebApp.blueprints import role_required
 
 
 @bp.route('/')
@@ -14,6 +15,8 @@ def index():
 @bp.post('/add')
 @bp.input(ReservationRequestSchema)
 @bp.output(ReservationResponseSchema)
+@bp.auth_required(auth)
+@role_required(["User","Administrator","Receptionist"])
 def add_reservation(json_data):
     success, result = ReservationService.reservation_add(json_data)
     if not success:
@@ -23,6 +26,8 @@ def add_reservation(json_data):
 @bp.put('/update/<int:rid>')
 @bp.input(ReservationUpdateSchema)
 @bp.output(ReservationResponseSchema)
+@bp.auth_required(auth)
+@role_required(["User","Administrator","Receptionist"])
 def update_reservation(rid, json_data):
     success, result = ReservationService.reservation_update(rid, json_data)
     if not success:
@@ -30,6 +35,8 @@ def update_reservation(rid, json_data):
     return result
 
 @bp.delete('/delete/<int:rid>')
+@bp.auth_required(auth)
+@role_required(["User","Administrator","Receptionist"])
 def delete_reservation(rid):
     success, result = ReservationService.reservation_delete(rid)
     if not success:
@@ -38,18 +45,28 @@ def delete_reservation(rid):
 
 @bp.get('/list')
 @bp.output(ReservationResponseSchema(many=True))
+@bp.auth_required(auth)
+@role_required(["Administrator","Receptionist"])
 def list_reservations():
-    success, result = ReservationService.reservation_list_all()
-    return result
+    success, response = ReservationService.reservation_list_all()
+    if success:
+        return response, 200
+    raise HTTPError(message=response, status_code=400)
 
-@bp.get('/user/<int:user_id>')
+@bp.get('/myreservation')
 @bp.output(ReservationResponseSchema(many=True))
-def list_reservations_by_user(user_id):
-    success, result = ReservationService.reservation_list_by_user(user_id)
-    return result
+@bp.auth_required(auth)
+@role_required(["User","Administrator","Receptionist"])
+def list_reservations_by_user():
+    success, response = ReservationService.reservation_list_by_user(auth.current_user.get("user_id"))
+    if success:
+        return response, 200
+    raise HTTPError(message=response, status_code=400)
 
 @bp.get('/rooms/<int:reservation_id>')
-@bp.output(RoomsListSchema(many=True))  
+@bp.output(RoomsListSchema(many=True))
+@bp.auth_required(auth)
+@role_required(["User","Administrator","Receptionist"])  
 def get_rooms_by_reservation(reservation_id):
     success, rooms_or_error = ReservationService.get_rooms_for_reservation(reservation_id)
     if not success:
@@ -58,6 +75,8 @@ def get_rooms_by_reservation(reservation_id):
 
 @bp.get("/<int:rid>")
 @bp.output(ReservationResponseSchema)
+@bp.auth_required(auth)
+@role_required(["User","Administrator","Receptionist"])  
 def get_reservation_by_id(rid):
     success, result = ReservationService.reservation_get_by_id(rid)
     if not success:
@@ -66,6 +85,8 @@ def get_reservation_by_id(rid):
 
 @bp.put('/cancel/<int:rid>')
 @bp.output(ReservationResponseSchema)
+@bp.auth_required(auth)
+@role_required(["User","Receptionist"])  
 def cancel_reservation(rid):
     success, result = ReservationService.reservation_cancel(rid)
     if not success:
