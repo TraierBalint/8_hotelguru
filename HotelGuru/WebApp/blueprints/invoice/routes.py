afc5176db1c02f0af39ae99c03201b3ec3a1fc2c
almaflex10 <ormos.barnabas10@gmail.com>
2025-04-26 09:37:07 +0200

    invoice auth kész, nem tudtam tesztelni mert fogalmam sincs milyen adatot vár(elméletben müködnie kell)

--- HotelGuru/WebApp/blueprints/invoice/routes.py
+++ HotelGuru/WebApp/blueprints/invoice/routes.py
@@ -3,6 +3,8 @@ from apiflask import HTTPError
 from WebApp.blueprints.invoice import bp
 from WebApp.blueprints.invoice.schemas import InvoiceRequestSchema, InvoiceResponseSchema ,InvoiceUpdateSchema
 from WebApp.blueprints.invoice.service import InvoiceService
+from WebApp.extensions import auth
+from WebApp.blueprints import role_required
 
 @bp.route('/')
 def index():
@@ -12,6 +14,8 @@ def index():
 @bp.post('/create')
 @bp.input(InvoiceRequestSchema, location='json')
 @bp.output(InvoiceResponseSchema)
+@bp.auth_required(auth)
+@role_required(["Administrator","Receptionist"])  
 def create_invoice(json_data):
     success, response = InvoiceService.create(json_data)
     if success:
@@ -22,6 +26,8 @@ def create_invoice(json_data):
 @bp.get("/<int:invoice_id>")
 @bp.doc(tags=["invoice"])
 @bp.output(InvoiceResponseSchema)
+@bp.auth_required(auth)
+@role_required(["Administrator","Receptionist"])
 def get_invoice(invoice_id):
     success, response = InvoiceService.get_by_id(invoice_id)
     if success:
@@ -31,6 +37,8 @@ def get_invoice(invoice_id):
 @bp.get("/list")
 @bp.doc(tags=["invoice"])
 @bp.output(InvoiceResponseSchema(many=True))
+@bp.auth_required(auth)
+@role_required(["Administrator","Receptionist"])
 def list_invoices():
     success, response = InvoiceService.list_all()
     if success:
@@ -41,6 +49,8 @@ def list_invoices():
 @bp.doc(tags=["invoice"])
 @bp.input(InvoiceUpdateSchema, location="json")
 @bp.output(InvoiceResponseSchema)
+@bp.auth_required(auth)
+@role_required(["Administrator","Receptionist"])
 def update_invoice(invoice_id, json_data):
     success, response = InvoiceService.update(invoice_id, json_data)
     if success:
@@ -49,6 +59,8 @@ def update_invoice(invoice_id, json_data):
 
 @bp.delete("/delete/<int:invoice_id>")
 @bp.doc(summary="Delete invoice", description="Deletes an invoice by ID", tags=["invoice"])
+@bp.auth_required(auth)
+@role_required(["Administrator","Receptionist"])
 def delete_invoice(invoice_id):
     success, response = InvoiceService.delete(invoice_id)
     if success:
